b18804e9c2349bb28ab4bf1c014f80d411e7ed0c
StoriesCao <cao4618077@126.com>
2022-08-24 10:18:12 +0800

    Update ncov.py

--- Github_Action/ncov.py
+++ Github_Action/ncov.py
@@ -45,6 +45,7 @@ params = urllib.parse.urlencode(result)
 with opener.open('https://app.upc.edu.cn/ncov/wap/default/save', data=bytes(params, 'utf-8')) as resp:
     print(resp.read().decode('utf-8'))
 
+print('hello')
 curr_time = datetime.datetime.now()
 response = requests.request('get', f'https://api.day.app/qjUhpKS9bJxkCyrsSxUzU5/签到成功!/UPC-疫情防控通已上报({curr_time.month}月{curr_time.day}日)')
 print(response)
